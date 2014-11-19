1cdebd1d2b52a189eb153a956556b91bbcac3e44
Elliot Winkler <elliot.winkler@gmail.com>
2014-11-19 16:13:59 -0700

    Consolidate set_session and set_flash APIs
    * set_session('key') changes to set_session['key']
    * set_flash['key'].to(nil) no longer works if set_flash has never been
      set

--- lib/shoulda/matchers/action_controller/set_session_matcher.rb
+++ lib/shoulda/matchers/action_controller/set_session_matcher.rb
@@ -1,3 +1,5 @@
+require 'forwardable'
+
 module Shoulda
   module Matchers
     module ActionController
@@ -5,45 +7,11 @@ module Shoulda
       # `session` hash.
       #
       #     class PostsController < ApplicationController
-      #       def show
-      #         session[:foo] = 'bar'
-      #       end
-      #     end
-      #
-      #     # RSpec
-      #     describe PostsController do
-      #       describe 'GET #show' do
-      #         before { get :show }
-      #
-      #         it { should set_session(:foo) }
-      #         it { should_not set_session(:baz) }
-      #       end
-      #     end
-      #
-      #     # Test::Unit
-      #     class PostsControllerTest < ActionController::TestCase
-      #       context 'GET #show' do
-      #         setup { get :show }
-      #
-      #         should set_session(:foo)
-      #         should_not set_session(:baz)
-      #       end
-      #     end
-      #
-      # #### Qualifiers
-      #
-      # ##### to
-      #
-      # Use `to` to assert that the key in the session hash was set to a
-      # particular value.
-      #
-      #     class PostsController < ApplicationController
       #       def index
-      #         session[:foo] = 'bar'
+      #         session[:foo] = 'A candy bar'
       #       end
       #
-      #       def show
-      #         session[:foo] = nil
+      #       def destroy
       #       end
       #     end
       #
@@ -52,15 +20,13 @@ module Shoulda
       #       describe 'GET #index' do
       #         before { get :index }
       #
-      #         it { should set_session(:foo).to('bar') }
-      #         it { should_not set_session(:foo).to('something else') }
-      #         it { should_not set_session(:foo).to(nil) }
+      #         it { should set_session }
       #       end
       #
-      #       describe 'GET #show' do
-      #         before { get :show }
+      #       describe 'DELETE #destroy' do
+      #         before { delete :destroy }
       #
-      #         it { should set_session(:foo).to(nil) }
+      #         it { should_not set_session }
       #       end
       #     end
       #
@@ -69,119 +35,128 @@ module Shoulda
       #       context 'GET #index' do
       #         setup { get :index }
       #
-      #         should set_session(:foo).to('bar')
-      #         should_not set_session(:foo).to('something else')
-      #         should_not set_session(:foo).to(nil)
+      #         should set_session
       #       end
       #
-      #       context 'GET #show' do
+      #       context 'DELETE #destroy' do
+      #         setup { delete :destroy }
+      #
+      #         should_not set_session
+      #       end
+      #     end
+      #
+      # #### Qualifiers
+      #
+      # ##### []
+      #
+      # Use `[]` to narrow the scope of the matcher to a particular key.
+      #
+      #     class PostsController < ApplicationController
+      #       def index
+      #         session[:foo] = 'A candy bar'
+      #       end
+      #     end
+      #
+      #     # RSpec
+      #     describe PostsController do
+      #       describe 'GET #index' do
+      #         before { get :index }
+      #
+      #         it { should set_session[:foo] }
+      #         it { should_not set_session[:bar] }
+      #       end
+      #     end
+      #
+      #     # Test::Unit
+      #     class PostsControllerTest < ActionController::TestCase
+      #       context 'GET #index' do
       #         setup { get :show }
       #
-      #         should set_session(:foo).to(nil)
+      #         should set_session[:foo]
+      #         should_not set_session[:bar]
+      #       end
+      #     end
+      #
+      # ##### to
+      #
+      # Use `to` to assert that some key was set to a particular value, or that
+      # some key matches a particular regex.
+      #
+      #     class PostsController < ApplicationController
+      #       def index
+      #         session[:foo] = 'A candy bar'
+      #       end
+      #     end
+      #
+      #     # RSpec
+      #     describe PostsController do
+      #       describe 'GET #index' do
+      #         before { get :index }
+      #
+      #         it { should set_session.to('A candy bar') }
+      #         it { should set_session.to(/bar/) }
+      #         it { should set_session[:foo].to('bar') }
+      #         it { should_not set_session[:foo].to('something else') }
+      #       end
+      #     end
+      #
+      #     # Test::Unit
+      #     class PostsControllerTest < ActionController::TestCase
+      #       context 'GET #index' do
+      #         setup { get :show }
+      #
+      #         should set_session.to('A candy bar')
+      #         should set_session.to(/bar/)
+      #         should set_session[:foo].to('bar')
+      #         should_not set_session[:foo].to('something else')
       #       end
       #     end
       #
       # @return [SetSessionMatcher]
       #
-      def set_session(key)
-        SetSessionMatcher.new(key)
+      def set_session
+        SetSessionMatcher.new.in_context(self)
       end
 
       # @private
       class SetSessionMatcher
-        def initialize(key)
-          @key = key.to_s
-          @value_block = nil
-        end
+        extend Forwardable
 
-        def to(value = nil, &block)
-          @value = value
-          @value_block = block
-          self
-        end
+        def_delegators :underlying_matcher,
+          :description,
+          :matches?,
+          :failure_message,
+          :failure_message_when_negated
+        alias_method \
+          :failure_message_for_should,
+          :failure_message
+        alias_method \
+          :failure_message_for_should_not,
+          :failure_message_when_negated
 
-        def matches?(controller)
-          @controller = controller
-          if @value_block
-            @value = @context.instance_eval(&@value_block)
-          end
-          assigned_correct_value? || cleared_value?
-        end
-
-        def failure_message
-          "Expected #{expectation}, but #{result}"
-        end
-        alias failure_message_for_should failure_message
-
-        def failure_message_when_negated
-          "Didn't expect #{expectation}, but #{result}"
-        end
-        alias failure_message_for_should_not failure_message_when_negated
-
-        def description
-          description = "set session variable #{@key.inspect}"
-          if @value
-            description << " to #{@value.inspect}"
-          end
-          description
+        def initialize
+          store = SessionStore.new
+          @underlying_matcher = SetSessionOrFlashMatcher.new(store)
         end
 
         def in_context(context)
-          @context = context
+          underlying_matcher.in_context(context)
           self
         end
 
-        private
-
-        def value_or_default_value
-          defined?(@value) && @value
+        def [](key)
+          underlying_matcher[key]
+          self
         end
 
-        def assigned_value?
-          !assigned_value.nil?
+        def to(expected_value = nil, &block)
+          underlying_matcher.to(expected_value, &block)
+          self
         end
 
-        def cleared_value?
-          defined?(@value) && @value.nil? && assigned_value.nil?
-        end
+        protected
 
-        def assigned_correct_value?
-          if assigned_value?
-            if !defined?(@value)
-              true
-            else
-              assigned_value == value_or_default_value
-            end
-          end
-        end
-
-        def assigned_value
-          session[@key]
-        end
-
-        def expectation
-          expectation = "session variable #{@key} to be set"
-
-          if value_or_default_value
-            expectation << " to #{value_or_default_value.inspect}"
-          end
-        end
-
-        def result
-          if session.empty?
-            'no session variables were set'
-          else
-            "the session was #{session.inspect}"
-          end
-        end
-
-        def session
-          if @controller.request.respond_to?(:session)
-            @controller.request.session.to_hash
-          else
-            @controller.response.session.data
-          end
-        end
+        attr_reader :underlying_matcher
       end
     end
   end

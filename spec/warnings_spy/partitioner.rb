ed1d741f02c9a1e011a57dd4736dbdc9ac0f90af
Elliot Winkler <elliot.winkler@gmail.com>
2014-11-21 12:11:32 -0700

    Fix

--- spec/warnings_spy/partitioner.rb
+++ spec/warnings_spy/partitioner.rb
@@ -13,9 +13,7 @@ class WarningsSpy
 
     def partition
       @relevant_warning_groups, @irrelevant_warning_groups =
-        warning_groups.partition do |group|
-          group[0].include?(search_text) && !group[0].include?('/gemfiles/')
-        end
+        warning_groups.partition { |group| relevant_warnings?(group) }
     end
 
     protected
@@ -25,5 +23,15 @@ class WarningsSpy
     private
 
     def_delegators :reader, :warning_groups
+
+    def relevant_warnings?(group)
+      first_line = group[0]
+
+      if first_line.include?('travis')
+        first_line.start_with?('/home/travis/build/thoughtbot/shoulda-matchers/lib')
+      else
+        first_line.include?('shoulda-matchers')
+      end
+    end
   end
 end
